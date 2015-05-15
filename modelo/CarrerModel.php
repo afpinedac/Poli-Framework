10d1da11ef7cf0c1fa56874ac207748c1ab485e0
andr3s2 <andr3s2@gmail.com>
2015-05-15 17:30:33 -0500

    Load the header (with bootstrap) and the footer

--- modelo/CarrerModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class CarrerModel extends MasterModel{
-    
-    
-    
-}
--- vistas/include/header.php
+++ vistas/include/header.php
@@ -2,6 +2,9 @@
   <head>
     <title>Mi Empresa.com</title>
     
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    
   </head>
   <body>
     
